55038247a7f578d708186d78b53f76e449538fb7
Pearl <80253713+Pearl-K@users.noreply.github.com>
2023-09-25 22:38:56 +0900

    Update Coriolis_force_update.py

--- NWP/Coriolis_force_update.py
+++ NWP/Coriolis_force_update.py
@@ -1,25 +1,24 @@
 import numpy as np
 
-# 문제 상황
-# 450kg 의 미사일이 29,000km/h의 속도로 우리나라에서 북쪽으로 발사되었을 때, 
-# 미사일에 작용하는 전향력은 얼마인가? ( 지구 각속도 : 7.29X10-5radians /sec )
+#mass and object velocity are arbitrary values.
+#질량, 물체의 속도는 임의의 값 입니다.
 
 def calculate_coriolis_force(mass, velocity, angular_velocity):
-    # 외적 (cross product) 계산
+    # cross product - 외적 계산 함수 np.cross
     coriolis_force = -2 * mass * np.cross(angular_velocity, velocity)
     return coriolis_force
 
-# 물체의 질량 (예: 1 kg)
-mass = 1.0
+# mass - 물체의 질량 (kg)
+mass = 100
 
-# 물체의 속도 벡터 V (예: [1.0, 2.0, 3.0] m/s, x, y, z 축 방향의 속도)
-velocity = np.array([1.0, 2.0, 3.0])
+# 물체의 속도 벡터 V (m/s)
+velocity = np.array([0.0, 2000, 0])
 
-# 지구의 각속도 벡터 Ω (북극 방향으로 가리킴, 예: [0.0, 0.0, Ωz] rad/s)
-angular_velocity = np.array([0.0, 0.0, Ωz])
+# Earth's angular velocity - 지구의 각속도 Ω (rad/s)
+angular_velocity = np.array([0.0, 0.0, 7.29e-5])
 
-# 코리올리 힘 계산
+# coriolis force - 코리올리 힘 계산
 coriolis_force = calculate_coriolis_force(mass, velocity, angular_velocity)
 
-# 결과 출력
-print("코리올리 힘:", coriolis_force)
+# print results
+print("Coriolis force:", coriolis_force)
